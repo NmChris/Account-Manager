1d7fbf6a5d680b4e225834e44f56c1decb445a1d
NmChris <nmchrisvitti@gmail.com>
2021-07-09 21:58:42 -0400

    Refactoring
    - Refactored code for new more compact design

--- Main.cpp
+++ Main.cpp
@@ -3,10 +3,8 @@
 #include "Identifiers.h"
 
 wxBEGIN_EVENT_TABLE(Main, wxFrame)
-	EVT_BUTTON(UID::ADD_ACCOUNT_BUTTON, AddAccount)
-	EVT_BUTTON(UID::REMOVE_ACCOUNT_BUTTON, RemoveAccount)
 	EVT_BUTTON(UID::LOGIN_BUTTON, Login)
-	EVT_LISTBOX(UID::ACCOUNT_LISTBOX, AccountSelected)
+	//EVT_LISTBOX(UID::ACCOUNT_LISTBOX, AccountSelected)
 	EVT_LISTBOX_DCLICK(UID::ACCOUNT_LISTBOX, AccountDoubleClicked)
 wxEND_EVENT_TABLE()
 
@@ -16,118 +14,101 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "Account manager", wxDefaultPosition, 
 	m_accountManager.getAccounts(&accountArray);
 
 	// Initialize the dialog popup
-	m_messageDialog = new wxMessageDialog(this, "", "Confirm the following action", wxYES_NO | wxCENTRE | wxSTAY_ON_TOP, wxDefaultPosition);
+	m_messageDialog = new wxMessageDialog(this, "Test messag", "Confirm the following action", wxYES_NO | wxCENTRE | wxSTAY_ON_TOP, wxDefaultPosition);
 
-	// Initialize frame panels
-	wxPanel* left_panel = new wxPanel(this, wxID_ANY);
-	wxPanel* right_panel = new wxPanel(this, wxID_ANY);
+
+	// Custom dialog
+	/*wxDialog* add_dialog = new wxDialog(this, wxID_ANY, "Add account");
+	add_dialog->ShowModal();
+
+	wxTextEntryDialog* test_entry = new wxTextEntryDialog(this, "Test message", "test caption");
+	test_entry->ShowModal();*/
+
+	wxMultipleTextDialog* test = new wxMultipleTextDialog(this, wxID_ANY, "Test");
+	test->ShowModal();
+
+	// Menu bar
+	wxMenuBar* menubar = new wxMenuBar();
+	wxMenu* account_menu = new wxMenu();
+
+	account_menu->Append(wxID_ANY, "Add");
+	account_menu->Append(wxID_ANY, "Remove");
+	account_menu->Append(wxID_ANY, "Edit");
+
+	menubar->Append(account_menu, "Account");
+	SetMenuBar(menubar);
 
 	// Initialize account list and login GUI elements
-	m_login = new wxButton(left_panel, UID::LOGIN_BUTTON, "Login", wxDefaultPosition, wxSize(300, 30));
-	m_accountList = new wxListBox(left_panel, UID::ACCOUNT_LISTBOX, wxDefaultPosition, wxSize(300, 300), accountArray, wxLB_SORT);
+	m_login = new wxButton(this, UID::LOGIN_BUTTON, "Login", wxDefaultPosition, wxSize(300, 30));
+	m_accountList = new wxListBox(this, UID::ACCOUNT_LISTBOX, wxDefaultPosition, wxSize(300, 300), accountArray, wxLB_SORT);
 
-	// Initialize account information elements
-	m_alias = new wxTextCtrl(right_panel, UID::ALIAS_TEXTBOX, "");
-	m_username = new wxTextCtrl(right_panel, UID::USERNAME_TEXTBOX, "");
-	m_password = new wxTextCtrl(right_panel, UID::PASSWORD_TEXTBOX, "", wxDefaultPosition, wxDefaultSize, wxTE_PASSWORD);
-	m_addAccount = new wxButton(right_panel, UID::ADD_ACCOUNT_BUTTON, "Add", wxDefaultPosition, wxSize(120, 30));
-	m_removeAccount = new wxButton(right_panel, UID::REMOVE_ACCOUNT_BUTTON, "Remove");
-
-	// Initialize static labels
-	wxStaticText* alias_label = new wxStaticText(right_panel, wxID_ANY, "Account alias");
-	wxStaticText* username_label = new wxStaticText(right_panel, wxID_ANY, "Username");
-	wxStaticText* password_label = new wxStaticText(right_panel, wxID_ANY, "Password");
-
+	
 	// Adjust panel background colors
-	left_panel->SetBackgroundColour(wxColor(171, 171, 171, 255)); // prev value 171
-	right_panel->SetBackgroundColour(wxColor(171, 171, 171, 255));
+	this->SetBackgroundColour(wxColor(171, 171, 171, 255));
 
 	// Initialize sizers
-	wxBoxSizer* panel_sizer = new wxBoxSizer(wxHORIZONTAL);
-	wxBoxSizer* info_sizer = new wxBoxSizer(wxVERTICAL);
 	wxBoxSizer* account_sizer = new wxBoxSizer(wxVERTICAL);
-	wxBoxSizer* button_sizer = new wxBoxSizer(wxHORIZONTAL);
-
-	// Add GUI elements to sizers
-	info_sizer->Add(m_accountList, wxSizerFlags(1).Expand().Border(wxALL, 10));
-	info_sizer->Add(m_login, wxSizerFlags(0).Expand().Border(wxALL, 10));
-
-	account_sizer->Add(alias_label, wxSizerFlags(0).Border(wxLEFT | wxRIGHT | wxTOP, 10));
-	account_sizer->Add(m_alias, wxSizerFlags(0).Expand().Border(wxLEFT | wxRIGHT | wxBOTTOM, 10));
-	account_sizer->Add(username_label, wxSizerFlags(0).Border(wxLEFT | wxRIGHT, 10));
-	account_sizer->Add(m_username, wxSizerFlags(0).Expand().Border(wxLEFT | wxRIGHT | wxBOTTOM, 10));
-	account_sizer->Add(password_label, wxSizerFlags(0).Border(wxLEFT | wxRIGHT, 10));
-	account_sizer->Add(m_password, wxSizerFlags(0).Expand().Border(wxLEFT | wxRIGHT | wxBOTTOM, 10));
-
-	button_sizer->Add(m_addAccount, wxSizerFlags(1).Expand().Border(wxALL, 10));
-	button_sizer->Add(m_removeAccount, wxSizerFlags(1).Expand().Border(wxALL, 10));
-
-	account_sizer->AddSpacer(163);
-	account_sizer->Add(button_sizer, wxSizerFlags().Bottom());
-
-	panel_sizer->Add(left_panel, wxSizerFlags(1).Expand().Border(wxRIGHT | wxBOTTOM, 10));
-	panel_sizer->Add(right_panel, wxSizerFlags(0).Expand().Border(wxRIGHT, 10));
+	account_sizer->Add(m_accountList, wxSizerFlags(1).Border());
+	account_sizer->Add(m_login, wxSizerFlags(0).Border(wxLEFT | wxRIGHT | wxBOTTOM));
 
 	// Apply sizers
 	SetSizerAndFit(account_sizer);
-	SetSizerAndFit(info_sizer);
-	SetSizerAndFit(panel_sizer);
 }
 
-void Main::AddAccount(wxCommandEvent &event) {
-	wxString alias		= m_alias->GetValue();
-	wxString username	= m_username->GetValue();
-	wxString password	= m_password->GetValue();
-
-	int alias_length	= alias.length();
-	int username_length = username.length();
-	int password_length = password.length();
-	
-	if (alias_length > 0 && username_length > 0 && password_length > 0) {
-		if (m_accountList->FindString(alias) == -1) {
-			m_accountList->AppendString(alias);
-			m_accountManager.addAccount(alias, username, password);
-		} else {
-			m_messageDialog->SetMessage("Are you sure you want to edit this account?");
-			if (m_messageDialog->ShowModal() == wxID_YES) {
-				m_accountManager.addAccount(alias, username, password);
-			}
-		}
-	} else {
-		wxString error_message = "Cannot add account. The following fields are empty: \n";
-
-		if (alias_length == 0)
-			error_message += "alias, ";
-		if (username_length == 0)
-			error_message += "username, ";
-		if (password_length == 0)
-			error_message += "password, ";
-
-		m_messageDialog->SetMessage(error_message.substr(0, error_message.length() - 2));
-		m_messageDialog->ShowModal();
-	}
-	event.Skip();
-}
-
-void Main::RemoveAccount(wxCommandEvent &event) {
-	const int account_index = m_accountList->GetSelection();
-	if (account_index >= 0) {
-		wxString alias = m_accountList->GetStringSelection();
-		m_messageDialog->SetMessage("Are you sure you want to remove " + alias + " from the account list?");
-		if (m_messageDialog->ShowModal() == wxID_YES) {
-			m_alias->Clear();
-			m_username->Clear();
-			m_password->Clear();
-
-			m_accountManager.removeAccount(alias);
-			m_accountList->Delete(account_index);
-		}
-	} else {
-		m_messageDialog->SetMessage("Select an account to remove.");
-		m_messageDialog->ShowModal();
-	}
-	event.Skip();
-}
+//void Main::AddAccount(wxCommandEvent &event) {
+//	wxString alias		= m_alias->GetValue();
+//	wxString username	= m_username->GetValue();
+//	wxString password	= m_password->GetValue();
+//
+//	int alias_length	= alias.length();
+//	int username_length = username.length();
+//	int password_length = password.length();
+//	
+//	if (alias_length > 0 && username_length > 0 && password_length > 0) {
+//		if (m_accountList->FindString(alias) == -1) {
+//			m_accountList->AppendString(alias);
+//			m_accountManager.addAccount(alias, username, password);
+//		} else {
+//			m_messageDialog->SetMessage("Are you sure you want to edit this account?");
+//			if (m_messageDialog->ShowModal() == wxID_YES) {
+//				m_accountManager.addAccount(alias, username, password);
+//			}
+//		}
+//	} else {
+//		wxString error_message = "Cannot add account. The following fields are empty: \n";
+//
+//		if (alias_length == 0)
+//			error_message += "alias, ";
+//		if (username_length == 0)
+//			error_message += "username, ";
+//		if (password_length == 0)
+//			error_message += "password, ";
+//
+//		m_messageDialog->SetMessage(error_message.substr(0, error_message.length() - 2));
+//		m_messageDialog->ShowModal();
+//	}
+//	event.Skip();
+//}
+//
+//void Main::RemoveAccount(wxCommandEvent &event) {
+//	const int account_index = m_accountList->GetSelection();
+//	if (account_index >= 0) {
+//		wxString alias = m_accountList->GetStringSelection();
+//		m_messageDialog->SetMessage("Are you sure you want to remove " + alias + " from the account list?");
+//		if (m_messageDialog->ShowModal() == wxID_YES) {
+//			m_alias->Clear();
+//			m_username->Clear();
+//			m_password->Clear();
+//
+//			m_accountManager.removeAccount(alias);
+//			m_accountList->Delete(account_index);
+//		}
+//	} else {
+//		m_messageDialog->SetMessage("Select an account to remove.");
+//		m_messageDialog->ShowModal();
+//	}
+//	event.Skip();
+//}
 
 void Main::Login(wxCommandEvent &event) {
 	if (m_accountList->GetSelection() >= 0) {
@@ -142,15 +123,15 @@ void Main::Login(wxCommandEvent &event) {
 	event.Skip();
 }
 
-void Main::AccountSelected(wxCommandEvent &event) {
-	wxString alias = m_accountList->GetStringSelection();
-	wxString username = m_accountManager.getAccountUsername(alias);
-	wxString password = m_accountManager.getAccountPassword(alias);
-
-	m_alias->SetValue(alias);
-	m_username->SetValue(username);
-	m_password->SetValue(password);
-}
+//void Main::AccountSelected(wxCommandEvent &event) {
+//	wxString alias = m_accountList->GetStringSelection();
+//	wxString username = m_accountManager.getAccountUsername(alias);
+//	wxString password = m_accountManager.getAccountPassword(alias);
+//
+//	m_alias->SetValue(alias);
+//	m_username->SetValue(username);
+//	m_password->SetValue(password);
+//}
 
 void Main::AccountDoubleClicked(wxCommandEvent &event) {
 	Login(event);
--- Main.h
+++ Main.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "wx/wx.h"
 #include "Account.h"
+#include "Dialog.h"
 
 class Main : public wxFrame {
 public:
@@ -9,19 +10,14 @@ public:
 public:
 	CAccountManager m_accountManager;
 
-	wxTextCtrl* m_alias			= nullptr;
-	wxTextCtrl* m_username		= nullptr;
-	wxTextCtrl* m_password		= nullptr;
-	wxButton*	m_addAccount	= nullptr;
-	wxButton*	m_removeAccount	= nullptr;
 	wxListBox*	m_accountList	= nullptr;
 	wxButton*	m_login			= nullptr;
 	wxMessageDialog* m_messageDialog = nullptr;
 
-	void AddAccount(wxCommandEvent &event);
-	void RemoveAccount(wxCommandEvent &event);
+	//void AddAccount(wxCommandEvent &event);
+	//void RemoveAccount(wxCommandEvent &event);
 	void Login(wxCommandEvent &event);
-	void AccountSelected(wxCommandEvent &event);
+	//void AccountSelected(wxCommandEvent &event);
 	void AccountDoubleClicked(wxCommandEvent &event);
 
 	wxDECLARE_EVENT_TABLE();
